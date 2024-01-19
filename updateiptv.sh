5fbe4d2ccf139152a46fcfaa4cba769c77b90cd9
tianya <24786545+tianyagogogo@users.noreply.github.com>
2024-01-19 15:37:22 +0800

    git commit

--- updateiptv.sh
+++ updateiptv.sh
@@ -7,4 +7,5 @@ cat  /root/ipiptv/CNTV.m3u >>/root/ipiptv/IPTV.m3u
 
 rm -rf /root/ipiptv/CNTV.m3u /root/ipiptv/CCTV.m3u
 
-cd /root/ipiptv && git config --system user.email "24786545+tianyagogogo@users.noreply.github.com" && git config --system user.name "tianyagogogo"  && git pull  && git commit -a -m 'git commit '  && git push origin main
+cd /root/ipiptv && git config --system user.email "24786545+tianyagogogo@users.noreply.github.com" && git config --system user.name "tianyagogogo"  && git pull && git add -A  && git commit -a -m 'git commit '  && git push origin main
+
